321efc103e600374bddb938c56964deb678692e4
Anurag22060 <anuragsawal03@gmail.com>
2024-10-14 10:17:46 +0530

    First Commit

---
+++ src/chattingapplication/Server.java
@@ -0,0 +1,207 @@
+package chattingapplication;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.io.*;
+
+public class Server implements ActionListener {
+//  16th- Declare JTextField text globally so we can use it in actionListener function
+
+    JTextField text;
+    JPanel a1;
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+    static DataOutputStream dout;
+    
+    Server() {
+
+//        3rd
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0, 0, 450, 70);
+        p1.setLayout(null); // setBound for Jlabel or any other object will only run if setLayout of below frmae layer is null.
+        f.add(p1);
+
+//        4th
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        // we need to scale image to display it properly 
+        Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        // we cannot directly pass i2 to JLabel so we will convert it to ImageIcon
+        ImageIcon i3 = new ImageIcon(i2);
+        //
+        JLabel back = new JLabel(i3);
+        back.setBounds(5, 20, 25, 25);
+        p1.add(back);
+
+//        6th - Now we want that after clicking on back button some action should be performed
+        back.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent ae) {
+                System.exit(0);
+            }
+        });
+
+//        7th - Creating profile icon
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50, 50);
+        p1.add(profile);
+
+//        8th - Video image
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30, 30);
+        p1.add(video);
+
+//        9th-next image
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+
+        JLabel phone = new JLabel(i12);
+        phone.setBounds(360, 20, 35, 30);
+        p1.add(phone);
+//        10th-more image
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+
+        JLabel morevert = new JLabel(i15);
+        morevert.setBounds(420, 20, 10, 25);
+        p1.add(morevert);
+
+//        11th- main use of JLabel
+        JLabel name = new JLabel("Anuwagh");
+        name.setBounds(110, 15, 100, 18);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        p1.add(name);
+//        12th- Status
+        JLabel status = new JLabel("Active Now");
+        status.setBounds(110, 35, 100, 18);
+        status.setForeground(Color.WHITE);
+        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
+        p1.add(status);
+
+//        Now we have to make completely new different panel for displaying chats below
+//        13th
+        a1 = new JPanel();
+        a1.setBounds(5, 75, 440, 450);
+        f.add(a1);
+
+//        14th- make message sending box
+        text = new JTextField();
+        text.setBounds(5, 530, 310, 40);
+        text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(text);
+//        15th- Creating send button
+        JButton send = new JButton("Send");
+        send.setBounds(320, 530, 123, 40);
+        send.setForeground(Color.WHITE);
+        // Add Action Listener to send message on click
+        send.addActionListener(this);
+        send.setBackground(new Color(7, 94, 84));
+        send.setFont(new Font("SAN_SERIF", Font.BOLD, 16));
+        f.add(send);
+
+//        1st step (swing needed)
+        f.setSize(450, 580);
+        f.setLocation(150, 40);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
+
+//        2nd
+        f.setVisible(true);
+    }
+
+    // Since we are implementingActionListener interface which contains an below abstract method so we need to implement abstract method here
+//     5th
+    public void actionPerformed(ActionEvent ae) {
+//        17th-When we click on Send button we want to display message
+        try{
+        String out = text.getText();
+
+        JPanel p2 = formatLabel(out);
+
+        a1.setLayout(new BorderLayout());
+
+        JPanel right = new JPanel(new BorderLayout());
+        right.add(p2, BorderLayout.LINE_END);
+        vertical.add(right);
+        vertical.add(Box.createVerticalStrut(15));
+
+        a1.add(vertical, BorderLayout.PAGE_START);
+//        21- send message
+        dout.writeUTF(out);
+
+//    18th- Now we want to reload the page after we click send button so message can be displayed.
+        //These can be done by calling object and various functions of JFrame class
+        text.setText("");
+
+        f.repaint();
+        f.invalidate();
+        f.validate();
+        }
+        catch(Exception e){
+        e.printStackTrace();
+        }
+    }
+//      19th- Formating
+
+    public static JPanel formatLabel(String out) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel output = new JLabel("<html><p style=\"width: 150px\">" + out + "</p></html>");
+        output.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        output.setBackground(new Color(37, 211, 102));
+        output.setOpaque(true);
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        panel.add(output);
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        JLabel time = new JLabel();
+        time.setText(sdf.format(cal.getTime()));
+
+        panel.add(time);
+        return panel;
+    }
+
+    public static void main(String[] args) {
+        new Server();
+//    20-- Create Sockets
+        try {
+            ServerSocket skt = new ServerSocket(6001);
+            while (true) {
+                Socket s = skt.accept();
+                DataInputStream din = new DataInputStream(s.getInputStream());
+                dout = new DataOutputStream(s.getOutputStream());
+
+                while (true) {
+                    String msg = din.readUTF();
+                    JPanel panel = formatLabel(msg);
+                    
+                    JPanel left = new JPanel(new  BorderLayout());
+                    left.add(panel, BorderLayout.LINE_START);
+                    vertical.add(left);
+                    f.validate();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
